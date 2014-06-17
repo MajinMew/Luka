59643b82d0f44e6e97c3b42c26fe6c62a8aed738
Daniel Caaz <DCaazy@gmail.com>
2014-06-17 11:49:21 -0700

    Automated push from Fractal

--- Plugins/Git.pl
+++ Plugins/Git.pl
@@ -5,6 +5,10 @@ addPlug('Git', {
   'dependencies' => ['Fancify','Core_Utilities'],
   'modules' => ['Sys::Hostname'],
   'description' => "This plugin was created to easily push updates from Luka onto the main branch.",
+  'utilities' => {
+    'add' => {
+    }
+  },
   'commands' => {
     '^Git push( .+)?$' => {
       'description' => "Pushes latest updates to Github",
@@ -17,8 +21,37 @@ addPlug('Git', {
         system('git add *.pl');
         system('git add *.bat');
         system('git commit -m "'.$message.'"');
-        system('git push');
-        &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},"Pushed latest updates to >>Github with message \"$message\"");
+        my @output = split /\n|\r/, `git push`;
+        foreach(@output) {
+          chomp($_);
+          lkDebug($_);
+          if(/fetch first$/) { $error = 1; }
+        }
+        if($error) {
+          &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},"Pushed failed. Need to \x04git pull\x04 first.");
+        }
+        else {
+          &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},"Pushed latest updates to >>Github with message \"$message\"");
+        }
+      }
+    },
+    '^Git pull$' => {
+      'description' => "Pushes latest updates to Github",
+      'access' => 3,
+      'tags' => ['utility'],
+      'code' => sub {
+        my @output = split /\n|\r/, `git pull`;
+        my @files = ();
+        foreach(@output) {
+          chomp($_);
+          lkDebug($_);
+          if(/\#\s+modified\:\s+(.+)$/) {
+            my $name = $1;
+            $name =~ s/.+[\\\/](.+)/$1/g;
+            push(@files,$1);
+          }
+        }
+        &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},"Pulled latest updates from >>Github with message");
       }
     },
     '^Git status$' => {
@@ -26,7 +59,7 @@ addPlug('Git', {
       'access' => 3,
       'tags' => ['utility'],
       'code' => sub {
-        system('git add *.pl');
+        system('git add *.pl'); system('git add *.bat');
         my @output = split /\n|\r/, `git status`;
         my @files = ();
         foreach(@output) {
