86775678e9b2d4ab5fed3a98f97ce5af31871133
Daniel Caaz <DCaazy@gmail.com>
2014-06-30 10:12:59 -0700

    Sysinfo update

--- Plugins/Sysinfo.pl
+++ Plugins/Sysinfo.pl
@@ -6,26 +6,24 @@ addPlug('System', {
   'dependencies' => ['Core_Command','Core_Utilities'],
   'utilities' => {
     'info' => sub {
-      my (@uname, $ret, $usr, $avg, $up);
-    
+      # Handle, Where
+      if($lk{os} !~ /linux/i) { &{$utility{'Fancify_say'}}($_[0],$_[1],"Sorry, nothing's been set for \x04$lk{os}\x04 yet."); return 0; }
+      my (@uname, @return, $usr, $avg, $up);
+      @return = ();
       @uname = (split ' ', `uname -a`)[0..2];
-      
-      $ret = "Host '@uname[1]', running @uname[0] @uname[2] - ";
+      push(@return, "[Host: \x04$uname[1]\x04]", "[Running: \x04$uname[0] $uname[2]\x04]");
       if (`uptime` =~ /^.*?up\s*(.*?),\s*(\d+) users?,.*: ([\d\.]+)/){
         ($usr, $avg) = ($2, $3);
         ($up = $1) =~ s/\s*days?,\s*|\+/d+/;
-        $ret .= "Up: $up; Users: $usr; Load: $avg; ";
+        push(@return,"[Uptime: \x04$up\x04]","[Users: \x04$usr\x04]","[Load: \x04$avg\x04]");
       }
       # Free space
-      $ret .= "Free:";
-      if (`free` =~ /Mem:\s*(\d*)\s*\d*\s*(\d*)/) { $ret .= " [Mem: " . int(.5 + $2/2**10) . "/" . int(.5 + $1/2**10) . " Mio]"; } # For compatibility: replace $1 with $2
-      if (`free` =~ /Swap:\s*(\d*)\s*\d*\s*(\d*)/) { $ret .= " [Swap: " . int(.5 + $2/2**10) . "/" . int(.5 + $1/2**10) . " Mio]"; } # For compatibility: replace $1 with $2
+      if (`free` =~ /Mem:\s*(\d*)\s*\d*\s*(\d*)/) { push(@return,"[Memory: \x04". int(.5 + $2/2**10) ."\x04/\x04". int(.5 + $1/2**10) ."\x04MiB]"); }
+      if (`free` =~ /Swap:\s*(\d*)\s*\d*\s*(\d*)/) { push(@return,"[Swap: \x04". int(.5 + $2/2**10) ."\x04/\x04". int(.5 + $1/2**10) ."\x04MiB]"); }
 
-      for (`df -m -x nfs -x smbfs -x none`) {
-        /^\/\S*\s*(\S*)\s*\S*\s*(\S*)\s*\S*\s*(\S*)/ and $ret .= " [$3: $2/$1 Mio]";
-      }
-      $ret .= ";";
-      return $ret;
+      for (`df -m -x nfs -x smbfs -x none`) { /^\/\S*\s*(\S*)\s*\S*\s*(\S*)\s*\S*\s*(\S*)/ and push(@return,"[$3: \x04$2\x04/\x04$1\x04MiB]"); }
+      &{$utility{'Fancify_say'}}($_[0],$_[1],join " ", @return);
+      return 1;
     },
   },
   'commands' => {
@@ -33,12 +31,7 @@ addPlug('System', {
       'description' => "Sets OAuth keys for Twitter usage.",
       'access' => 3,
       'code' => sub {
-        if($lk{os} =~ /^linux$/i) {
-          &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},&{$utility{'System_info'}}());
-        }
-        else {
-          &{$utility{'Fancify_say'}}($_[1]{irc},$_[2]{where},"Sorry, nothing's been set for \x04$lk{os}\x04 yet.");
-        }
+        &{$utility{'System_info'}}($_[1]{irc},$_[2]{where});
       }
     }
   },
